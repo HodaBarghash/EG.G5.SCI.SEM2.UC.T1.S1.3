824a60f8a79f47474bcaca1acc5953a79d66216b
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-03-01 16:44:46 +0200

    Initial commit

---
+++ story_content/user.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "660tTyAW1pZ":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbyF0z0Z7Qhw0YuSogBsgnl0eItPTrRFCdQXaFZFoiOnuUW8UcYzDKN_ci6igKLORGjW/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({q1attempts:player.GetVar("q1attempts"),q1answeredcorrect:player.GetVar("q1answeredcorrect"),q2attempts:player.GetVar("q2attempts"),q2answeredcorrect:player.GetVar("q2answeredcorrect")})
+	}
+	)
+}
+
